9ec8cc44b53c2dae53dee5aab71844e00d9085b3
Hugo Ramillon <hugo.ramillon@etu.u-bordeaux.fr>
2026-05-05 19:18:00 +0200

    wgan py final

--- models/wgan-gp.py
+++ models/wgan-gp.py
@@ -1,27 +1,34 @@
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers, models, Model
-import tensorflow.keras.backend as K
+from tensorflow.keras import layers, models
 import numpy as np
 import yaml
 import matplotlib.pyplot as plt
-from tensorflow.keras import losses, metrics
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import mixed_precision
+# --- CONFIGURATION ---
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            # Permet de ne consommer que la VRAM nécessaire
+            tf.config.experimental.set_memory_growth(gpu, True)
+        print(f"GPU détecté et activé : {gpus[0]}")
+    except RuntimeError as e:
+        print(e)
 
 with open("configs/cat_wgan.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-latent_dim = config['model_params']['latent_dim']
-conv_filters = config['model_params']['conv_filters']
-k = config['model_params']['kernel_size']
+latent_dim = config['model_params']['latent_dim'] 
 epochs = config['training_params']['epochs']
-learning_rate = config['training_params']['learning_rate']
 ressources = config['paths']['ressources']
 batch_size = config['training_params']['batch_size']
 
+# --- DATASET ---
 train_data = tf.keras.utils.image_dataset_from_directory(
     ressources,
     labels=None,
@@ -32,75 +39,58 @@ train_data = tf.keras.utils.image_dataset_from_directory(
     seed=42
 )
 
-data_augmentation = tf.keras.Sequential([
-    layers.RandomFlip("horizontal"),
-    layers.RandomRotation(0.05),
-])
-
 def preprocess(img):
-    img = (tf.cast(img, "float32") - 127.5) / 127.5
-    return img
+    # Normalisation entre -1 et 1 (standard pour WGAN)
+    return (tf.cast(img, "float32") - 127.5) / 127.5
 
-train = train_data.map(preprocess).map(lambda x: data_augmentation(x, training=True))
+train = train_data.map(preprocess)
 train = train.prefetch(buffer_size=tf.data.AUTOTUNE)
 
+# --- ARCHITECTURE ---
 def build_generator(latent_dim):
     model = models.Sequential(name="generator")
-    
     model.add(layers.Input(shape=(latent_dim,)))
-    model.add(layers.Dense((4* 4*512)))
-    model.add(layers.Reshape((4,4,512)))
     
-    model.add(layers.UpSampling2D(size=(2, 2), interpolation="bilinear"))
-    model.add(layers.Conv2D(256, 3, padding="same"))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU(0.2))
+    model.add(layers.Dense(4 * 4 * 1024))
+    model.add(layers.Reshape((4, 4, 1024)))
+    model.add(layers.ReLU())
+
+    for filters in [512, 256, 128]:
+        model.add(layers.UpSampling2D(size=(2, 2)))
+        model.add(layers.Conv2D(filters, kernel_size=3, padding="same"))
+        model.add(layers.BatchNormalization())
+        model.add(layers.ReLU())
+
+    model.add(layers.UpSampling2D(size=(2, 2)))
+    model.add(layers.Conv2D(3, kernel_size=3, padding="same", activation="tanh"))
     
-    model.add(layers.UpSampling2D(size=(2, 2), interpolation="bilinear"))
-    model.add(layers.Conv2D(128, 3, padding="same"))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU(0.2))
-    
-    model.add(layers.UpSampling2D(size=(2, 2), interpolation="bilinear"))
-    model.add(layers.Conv2D(64, 3, padding="same"))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU(0.2))
-    
-    model.add(layers.UpSampling2D(size=(2, 2), interpolation="bilinear"))
-    model.add(layers.Conv2D(3, 3, padding="same", activation="tanh"))
     return model
 
 def build_critic():
     model = models.Sequential(name="critic")
-    model.add(layers.Input(shape=(64,64,3)))
+    model.add(layers.Input(shape=(64, 64, 3)))
     
-    model.add(layers.Conv2D(64, 4, strides=2, padding="same", input_shape=(64, 64, 3)))
+    model.add(layers.Conv2D(64, 4, strides=2, padding="same"))
     model.add(layers.LeakyReLU(0.2))
     
     model.add(layers.Conv2D(128, 4, strides=2, padding="same"))
-    model.add(layers.LayerNormalization()) 
+    # model.add(layers.LayerNormalization()) # Optionnel, retire-le si ça diverge encore
     model.add(layers.LeakyReLU(0.2))
     
     model.add(layers.Conv2D(256, 4, strides=2, padding="same"))
-    model.add(layers.LayerNormalization())
     model.add(layers.LeakyReLU(0.2))
     
-    model.add(layers.Conv2D(256, 3, strides=1, padding="same")) 
-    model.add(layers.LayerNormalization())
-    model.add(layers.LeakyReLU(0.2))
-    model.add(layers.Dropout(0.3)) 
-
     model.add(layers.Conv2D(512, 4, strides=2, padding="same"))
-    model.add(layers.LayerNormalization())
     model.add(layers.LeakyReLU(0.2))
     
     model.add(layers.Flatten())
-    model.add(layers.Dense(1)) 
-    
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(1))
     return model
 
+# --- WGAN-GP LOGIC ---
 class WGAN(keras.Model):
-    def __init__(self, generator, critic, latent_dim, gp_weight=20.0, n_critic=5):
+    def __init__(self, generator, critic, latent_dim, gp_weight=10.0, n_critic=8):
         super().__init__()
         self.generator = generator
         self.critic = critic
@@ -117,50 +107,54 @@ class WGAN(keras.Model):
 
     def gradient_penalty(self, batch_size, real_images, fake_images):
         alpha = tf.random.uniform([batch_size, 1, 1, 1], 0.0, 1.0)
-        diff = fake_images - real_images
-        interpolated = real_images + alpha * diff
-
+        interpolated = real_images + alpha * (fake_images - real_images)
+        
         with tf.GradientTape() as gp_tape:
             gp_tape.watch(interpolated)
             pred = self.critic(interpolated, training=True)
         
         grads = gp_tape.gradient(pred, [interpolated])[0]
-        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
-        gp = tf.reduce_mean((norm - 1.0) ** 2)
-        return gp
+        # Epsilon à 1e-12 pour la précision mathématique
+        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]) + 1e-12)
+        return tf.reduce_mean((norm - 1.0) ** 2)
 
     def train_step(self, real_images):
         batch_size = tf.shape(real_images)[0]
-
+        
+        # 1. Entraînement du Critic (toujours n_critic fois)
         for i in range(self.n_critic):
-            random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+            z = tf.random.normal(shape=(batch_size, self.latent_dim))
             with tf.GradientTape() as tape:
-                fake_images = self.generator(random_latent_vectors, training=True)
+                fake_images = self.generator(z, training=True)
                 fake_logits = self.critic(fake_images, training=True)
                 real_logits = self.critic(real_images, training=True)
-
+                
                 cost = tf.reduce_mean(fake_logits) - tf.reduce_mean(real_logits)
                 gp = self.gradient_penalty(batch_size, real_images, fake_images)
                 c_loss = cost + gp * self.gp_weight
+                
+            grads = tape.gradient(c_loss, self.critic.trainable_variables)
+            # Suppression du clip_by_global_norm : la GP s'occupe de la contrainte !
+            self.c_optimizer.apply_gradients(zip(grads, self.critic.trainable_variables))
 
-            c_grad = tape.gradient(c_loss, self.critic.trainable_variables)
-            self.c_optimizer.apply_gradients(zip(c_grad, self.critic.trainable_variables))
-
-        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+        # 2. Entraînement du Générateur
+        z = tf.random.normal(shape=(batch_size, self.latent_dim))
         with tf.GradientTape() as tape:
-            generated_images = self.generator(random_latent_vectors, training=True)
-            gen_img_logits = self.critic(generated_images, training=True)
-            g_loss = -tf.reduce_mean(gen_img_logits)
-
-        g_grad = tape.gradient(g_loss, self.generator.trainable_variables)
-        self.g_optimizer.apply_gradients(zip(g_grad, self.generator.trainable_variables))
+            generated_images = self.generator(z, training=True)
+            gen_logits = self.critic(generated_images, training=True)
+            g_loss = -tf.reduce_mean(gen_logits)
+            
+        grads = tape.gradient(g_loss, self.generator.trainable_variables)
+        # Suppression du clipping manuel ici aussi pour laisser le flot de gradient naturel
+        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_variables))
         
         self.c_loss_metric.update_state(c_loss)
         self.g_loss_metric.update_state(g_loss)
         return {"c_loss": self.c_loss_metric.result(), "g_loss": self.g_loss_metric.result()}
 
+# --- MONITORING (MODIFIÉ POUR 5 EPOCHS) ---
 class GANMonitor(keras.callbacks.Callback):
-    def __init__(self, num_img=16, latent_dim=200):
+    def __init__(self, num_img=16, latent_dim=100):
         super().__init__()
         self.num_img = num_img
         self.latent_dim = latent_dim
@@ -178,23 +172,23 @@ class GANMonitor(keras.callbacks.Callback):
                 plt.axis('off')
             plt.savefig(f"training/gen_img_epoch_{epoch+1}.png")
             plt.close()
-            # Sauvegarde robuste du générateur
+            # Sauvegarde du checkpoint
             self.model.generator.save(f"training/wgan_gen_checkpoints/gen_epoch_{epoch+1}.keras")
+            self.model.critic.save(f"training/wgan_cri_checkpoints/cri_epoch_{epoch+1}.keras")
 
+# --- EXECUTION ---
 os.makedirs("training/wgan_gen_checkpoints", exist_ok=True)
 
 generator = build_generator(latent_dim)
 critic = build_critic()
-
-wgan = WGAN(generator=generator, critic=critic, latent_dim=latent_dim, n_critic=10)
+wgan = WGAN(generator=generator, critic=critic, latent_dim=latent_dim)
 
 wgan.compile(
-    g_optimizer=keras.optimizers.Adam(learning_rate=0.00005, beta_1=0.0, beta_2=0.9),
-    c_optimizer=keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.0, beta_2=0.9)
+    g_optimizer=Adam(learning_rate=0.0001, beta_1=0.0, beta_2=0.9),
+    c_optimizer=Adam(learning_rate=0.0001, beta_1=0.0, beta_2=0.9)
 )
 
 img_monitor = GANMonitor(num_img=16, latent_dim=latent_dim)
-
 history = wgan.fit(train, epochs=epochs, callbacks=[img_monitor])
 
 save_gen_path = config['paths']['save_generator']
